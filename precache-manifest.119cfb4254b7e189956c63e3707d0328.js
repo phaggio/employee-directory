d73be0f1757cb454fb21e6b2898f1b970b10d441
Richard Wang <richardchacha@hotmail.com>
2020-04-12 15:40:53 -0700

    Updates

---
+++ precache-manifest.119cfb4254b7e189956c63e3707d0328.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9794674b5576791799b2150e32b19db2",
+    "url": "/employee-directory/index.html"
+  },
+  {
+    "revision": "4a2983dcde7870b076a1",
+    "url": "/employee-directory/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "def7fccafed23dde16cd",
+    "url": "/employee-directory/static/js/2.935742f2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/employee-directory/static/js/2.935742f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a2983dcde7870b076a1",
+    "url": "/employee-directory/static/js/main.73e72170.chunk.js"
+  },
+  {
+    "revision": "1555ac21ead60afe404d",
+    "url": "/employee-directory/static/js/runtime-main.2264778b.js"
+  }
+]);
--- src/utils/headerNames.js
+++
@@ -1,9 +0,0 @@
-export const headerNames = [
-  `ID`,
-  `Image`,
-  `Name`,
-  `Phone`,
-  `Email`,
-  `DOB`,
-  `Age`
-];
